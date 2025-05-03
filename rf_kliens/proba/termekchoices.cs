6b49c6c2c91a73dd3893699f011fcf771189caa7
mlevente0703 <mlevente0703@gmail.com>
2025-05-03 12:43:48 +0200

    Osztályok létrehozása a továbbiakhoz

---
+++ rf_kliens/proba/termekchoices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba
+{
+    public class Termekchoices
+    {
+        public string Name { get; set; }
+        public string Bvin { get; set; }
+        public int StoreId { get; set; }
+        //public string OptionType { get; set; }
+    }
+}
