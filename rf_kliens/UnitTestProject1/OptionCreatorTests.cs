61316beb4502bbe67ee2a747823b4a69c5b7dc50
onlyux <rajmund.does.business@gmail.com>
2025-05-05 08:24:12 +0200

    Megírtam az OptionCreatorTests.cs file tartalmát, de itt nem lehet valójában mockolni, mert a CreateOption metódusban nincs külön API-hívás

--- rf_kliens/UnitTestProject1/OptionCreatorTests.cs
+++ rf_kliens/UnitTestProject1/OptionCreatorTests.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Moq;
+using Moq.Protected;
+using Hotcakes.CommerceDTO.v1.Client;
+using YourNamespace.Managers;
 
-namespace UnitTestProject1
+namespace UniTestProject1.API
 {
-    internal class OptionCreatorTests
+    [TestFixture]
+    public class OptionCreatorTests
     {
+        private Api _dummyApi;
+        private Mock<OptionCreator> _mockCreator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dummyApi = new Api("https://fake", "KEY");
+            _mockCreator = new Mock<OptionCreator>() { CallBase = true };
+        }
+
+        [Test]
+        public void CreateOption_ReturnsTrue_WhenSuccessful()
+        {
+            // Arrange
+            var mock = new Mock<OptionCreator>("https://fakeurl", "APIKEY") { CallBase = true };
+
+            // Mivel a metódus nem ad vissza konkrét eredményt, csak boolt,
+            // itt nincs mit mockolni külön, ha nem használunk partial mockot
+
+            // Act
+            var result = mock.Object.CreateOption(
+                "Size",
+                "Small",
+                "Medium",
+                "Large",
+                "Required",
+                "True"
+            );
+
+            // Assert
+            Assert.That(result, Is.True); // vagy .Is.False ha hibát szimulálsz
+        }
+
     }
 }
